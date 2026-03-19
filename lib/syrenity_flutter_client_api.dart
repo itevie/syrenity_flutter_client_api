6cd4da54cca44e97d0419729158e99cf246ecd7d
Isabella <izzyplays07@gmail.com>
2026-03-19 11:50:51 +0000

    add updates

--- lib/syrenity_flutter_client_api.dart
+++ lib/syrenity_flutter_client_api.dart
@@ -4,5 +4,7 @@ export "src/client.dart";
 export "src/events.dart";
 export "src/dispatch_messages.dart";
 
+export "src/content_parser/parser.dart";
+
 export 'src/models/models.dart';
 export 'src/managers/managers.dart';

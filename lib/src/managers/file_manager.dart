057cb65eb29e4ce8a5715c70d9e4a268d099eeb8
Isabella <izzyplays07@gmail.com>
2026-05-13 22:58:18 +0100

    add updates

--- lib/src/managers/file_manager.dart
+++ lib/src/managers/file_manager.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:syrenity_flutter_client_api/syrenity_flutter_client_api.dart';
 
 class SyFileManager {
@@ -8,15 +9,14 @@ class SyFileManager {
 
   SyFileManager(this.client);
 
-  Future<SyFile> upload(File file) async {
-    final bytes = await file.readAsBytes();
-    final base64Data = base64Encode(bytes);
+  Future<SyFile> upload(PlatformFile file) async {
+    final bytes = file.bytes ?? await File(file.path!).readAsBytes();
 
-    final fileName = file.path.split(Platform.pathSeparator).last;
+    final base64Data = base64Encode(bytes);
 
     return await client.http.post<SyFile, Map<String, dynamic>>("/api/files", {
       'data': base64Data,
-      'file_name': fileName,
+      'file_name': file.name,
     }, (c, v) => SyFile.build(c, v));
   }
 }

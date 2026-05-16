7fda6075be502e9a79e7abf8097b5157c5123c12
Isabella <izzyplays07@gmail.com>
2026-05-16 17:42:33 +0100

    add updates

--- lib/src/models/message_ack.dart
+++ lib/src/models/message_ack.dart
@@ -1,17 +1,22 @@
+import 'package:syrenity_flutter_client_api/syrenity_flutter_client_api.dart';
+
 class SyMessageAck {
+  final SyrenityClient client;
+
   final int channelId;
   final int userId;
   final int? messageId;
   final DateTime acknowledgedAt;
 
   SyMessageAck({
+    required this.client,
     required this.channelId,
     required this.userId,
     required this.messageId,
     required this.acknowledgedAt,
   });
 
-  factory SyMessageAck.build(Map<String, dynamic> json) {
+  factory SyMessageAck.build(SyrenityClient client, Map<String, dynamic> json) {
     final acknowledgedAtRaw = json['acknowledged_at'];
     final acknowledgedAt =
         acknowledgedAtRaw is DateTime
@@ -19,6 +24,7 @@ class SyMessageAck {
             : DateTime.parse(acknowledgedAtRaw as String);
 
     return SyMessageAck(
+      client: client,
       channelId: json['channel_id'] as int,
       userId: json['user_id'] as int,
       messageId: json['message_id'] as int?,
@@ -33,5 +39,13 @@ class SyMessageAck {
       'message_id': messageId,
       'acknowledged_at': acknowledgedAt.toIso8601String(),
     };
+  }
+
+  Future<void> updateAck(int messageId) async {
+    await client.http.patch(
+      "/api/channels/$channelId/messages/$messageId/read",
+      {},
+      null,
+    );
   }
 }
